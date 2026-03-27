1e1a56a8b40cde237ab7b3f969e4bd464de8006c
AlefWilliam <151478411+AlefWilliam@users.noreply.github.com>
2026-03-26 23:48:29 -0300

    Update main.cpp

--- Bridge/main.cpp
+++ Bridge/main.cpp
@@ -1,1 +1,25 @@
+#include "Livro.h"
+#include "Revista.h"
+#include "PublicacaoImplBD.h"
+#include "PublicacaoImplXML.h"
 
+int main() {
+
+    Implementador* implBD = new PublicacaoImplBD();
+    Implementador* implXML = new PublicacaoImplXML();
+
+    Publicacao* livro = new Livro(implBD);
+    Publicacao* revista = new Revista(implXML);
+
+    livro->obterDados("Livro");
+    livro->getTitulo();
+    livro->getAutor();
+
+    cout << "------------------" << endl;
+
+    revista->obterDados("Revista");
+    revista->getTitulo();
+    revista->getAutor();
+
+    return 0;
+}
